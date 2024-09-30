566916704e971d524c6df3b7cae76b83d468261b
Jonas Grønquist <jog002@edu.zealand.dk>
2024-09-30 10:18:20 +0200

    Add project files.

---
+++ Obligatorisk Json client/Input.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonOPG
+{
+    public class Input
+    {
+        public string Method { get; set; }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+
+        public Input()
+        {
+
+        }
+
+
+        public Input(string method, int num1, int num2)
+        {
+            Method = method;
+            Num1 = num1;
+            Num2 = num2;
+
+        }
+
+    }
+}
---
+++ Obligatorisk Json client/Program.cs
@@ -0,0 +1,50 @@
+﻿using JsonOPG;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.Json;
+
+Console.WriteLine("This is the TCP Client");
+
+bool keepSending = true;
+
+TcpClient socket = new TcpClient("127.0.0.1", 7);
+
+
+NetworkStream ns = socket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+while (keepSending)
+{
+
+    Console.WriteLine("Input message, Example: add 15 15");
+    string message = Console.ReadLine().ToLower();
+    string[] parts = message.Split(' ');
+    if (message.ToLower() == "stop")
+    {
+        writer.WriteLine(message);
+        keepSending = false;
+    }
+    if (parts.Length == 3 &&
+        int.TryParse(parts[1], out int num1) &&
+        int.TryParse(parts[2], out int num2))
+    {
+        
+        var method = parts[0];
+
+        Input jsonMsg = new Input(method, num1, num2);
+        string jsonMessage = JsonSerializer.Serialize(jsonMsg);
+
+        writer.WriteLine(jsonMessage);
+    }
+
+    writer.Flush();
+    string response = reader.ReadLine();
+
+    Console.WriteLine("Server response: " + response);
+
+    if (message.ToLower() == "stop")
+    {
+        keepSending = false;
+    }
+}
